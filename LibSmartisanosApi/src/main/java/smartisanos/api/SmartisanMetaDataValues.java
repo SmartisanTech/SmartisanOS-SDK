42f74a7b9c83ccd401f21906c8dfccba3a22d0d1
zhangji <zhangji@smartisan.com>
2016-11-07 18:46:15 +0800

    SmartisanSDK: add smartisan feature tags.
    ticket:0
    
    Change-Id: I5772c09404e1823c0907ca85f1548280fe6e7dbc
    Signed-off-by: zhangji <zhangji@smartisan.com>

---
+++ LibSmartisanosApi/src/main/java/smartisanos/api/SmartisanMetaDataValues.java
@@ -0,0 +1,21 @@
+package smartisanos.api;
+
+/**
+ * @author Smartisan
+ */
+public class SmartisanMetaDataValues {
+
+    /**
+     * Set this meta-data name in AndroidManifest.xml to enable smartisan features,
+     */
+    public static final String FEATURE_KEY= "Smartisanos_Features";
+    /**
+     * Set this meta-data value in AndroidManifest.xml to support smartisan one-step.
+     */
+    public static final String FEATURE_ONE_STEP = "onestep";
+    /**
+     * Set this meta-data value in AndroidManifest.xml to support smartisan big-boom.
+     */
+    public static final String FEATURE_BIG_BOOM = "bigboom";
+    
+}
